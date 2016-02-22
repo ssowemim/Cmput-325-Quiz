2609ef9549b704b663f383064127878e096402f9
ssowemim <ssowemim@ualberta.ca>
2016-02-22 16:22:59 -0700

    midterm review

--- midterm.lisp
+++ midterm.lisp
@@ -1,9 +1,17 @@
 (defun t1 (L)
 	(if (null L)
+		; first argument
 		T
+		; second argument
 		(if (null (cdr L))
+			; first argument
 			(car L)
+			; second argument
 			(t1 (cddr L))
 		)
 	)	
 )
+
+#|
+	Even lists returns a T value, while odd lists returns the last list value
+|#
